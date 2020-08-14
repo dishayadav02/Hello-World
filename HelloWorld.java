a17d85fb7f6f487c0a3ca4fd3170fd96db051b6e
dishayadav02 <69659083+dishayadav02@users.noreply.github.com>
2020-08-13 21:13:02 -0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("I like to watch TV.");
+		System.out.println("I like to play minecraft.");
+		System.out.println("I like to play basketball.");
+		System.out.println("I want to major in bio, I think.");
+		System.out.println("My favorite TV show is In the Dark.");
+
+	}
+
+}
